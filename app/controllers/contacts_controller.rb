248f1c5d0614ed842565147b5fd3267e1cacf96d
Rommel Ortiz-Luis <rortizluis.pgm@gmail.com>
2015-11-25 02:16:30 +0000

    sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,22 +3,22 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-        #TODO send message
-        flash[:notice] = "Message sent from #{@contact.name}."
-        redirect_to root_path
+      UserMailer.contact_email(@contact).deliver
+      flash[:notice] = "Message sent from #{@contact.name}."
+      redirect_to root_path
     else
-        render :new
+      render :new
     end
   end
-  
+
   private
-  
+
   def secure_params
     params.require(:contact).permit(:name, :email, :content)
-  end  
+  end
 
 end
